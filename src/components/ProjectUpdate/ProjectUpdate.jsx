3f9f2c6cc773526301d610db9f12435802bd4957
Luis <luimd0728@gmail.com>
2021-09-25 19:44:24 -0400

    added project update section

--- src/components/ProjectUpdate/ProjectUpdate.jsx
+++ src/components/ProjectUpdate/ProjectUpdate.jsx
@@ -1,108 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { ConditionallyRender } from "react-util-kit";
+import React from "react";
 
-import "./ProjectUpdate.css";
+import Options from "../Options/Options";
 
-const ProjectUpdate = ({ selectedProject, setState, actionProvider }) => {
-  const [displaySelector, toggleDisplaySelector] = useState(true);
-  const [projects, setProjects] = useState([]);
-
-  const ProjectUpdate = [
+const ProjectUpdate = props => {
+  const options = [
     {
-      iata: "OSL",
-      nameCompact: "Oslo",
+      name: "View new Updates",
+      handler: props.actionProvider.handleViewUpdate,
+      id: 1
     },
     {
-      iata: "BOO",
-      nameCompact: "Bodo",
-    },
-    {
-      iata: "BGO",
-      nameCompact: "Bergen",
-    },
-    {
-      iata: "KRS",
-      nameCompact: "Kristiansand",
-    },
-    {
-      iata: "SVG",
-      nameCompact: "Stavanger",
-    },
-    {
-      iata: "TOS",
-      nameCompact: "Tromso",
-    },
-    {
-      iata: "TRD",
-      nameCompact: "Trondheim",
-    },
-    {
-      iata: "AES",
-      nameCompact: "Alesund",
-    },
+      name: "Make a new update",
+      handler: props.actionProvider.handleNewUpdate,
+      id: 2
+    }
   ];
-
-  useEffect(() => {
-    setProjects(ProjectUpdate);
-  }, []);
-
-  const handleSubmit = (e) => {
-    setState((state) => ({
-      ...state,
-      selectedProjects: projects.find(
-        (project) => project.iata === e.target.value
-      ),
-    }));
-  };
-
-  const handleConfirm = () => {
-    actionProvider.handleOptions();
-    toggleDisplaySelector((prevState) => !prevState);
-  };
-
-  if (!projects) return null;
-
-  const createProjectOptions = () => {
-    return projects.map((item) => {
-      return (
-        <option key={item.iata} value={item.iata}>
-          {item.nameCompact}
-        </option>
-      );
-    });
-  };
-
-  return (
-    <div className="project-selector-container">
-      <ConditionallyRender
-        ifTrue={displaySelector}
-        show={
-          <>
-            {" "}
-            <h2 className="project-selector-heading">Project</h2>
-            <select
-              className="project-selector"
-              value={selectedProject.iata}
-              onChange={handleSubmit}
-            >
-              {createProjectOptions()}
-            </select>
-            <button className="Project-button-confirm" onClick={handleConfirm}>
-              Confirm
-            </button>
-          </>
-        }
-        elseShow={
-          <>
-            <h2 className="project-selector-heading">Project</h2>
-            <p>
-              Great! You have chosen this Project: {selectedProject.nameCompact}
-            </p>
-          </>
-        }
-      />
-    </div>
-  );
+  return <Options options={options} title="Project Update Options" {...props} />;
 };
 
 export default ProjectUpdate;
