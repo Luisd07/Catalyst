6e08c5ef4a352ef30968630087c4c680f120ad51
Luis <luimd0728@gmail.com>
2021-09-25 20:56:30 -0400

    quality of life changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import './App.css';
 import Chatbot from 'react-chatbot-kit'
 
-import actionProvider from './actionProvider'
-import messageParser from './messageParser'
-
 import config from "./config";
 import MessageParser from "./messageParser";
 import ActionProvider from "./actionProvider";
